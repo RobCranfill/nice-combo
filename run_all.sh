691e3489f2890b298a472f09a461ce00e3c9c059
Rob Cranfill <robcranfill@gmail.com>
2018-03-28 12:08:25 -0700

    Towards removing multiple scripts

---
+++ run_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Do all the scripts.
+# Not necessarily what you'll want to do every time,
+# but it shows you all the required steps.
+
+# Input param for this script: the dictionary file to use,
+#  such as /usr/share/dict/american-english-huge (ok, but lots of garbage)
+#  or something from http://wordlist.aspell.net/ (better quality words)
+
+DICT_FILE=$1
+
+WORDS_IN=words.in.text
+WORDS_OUT=words.out.text
+HTML_OUT=combo.html
+
+# Find all 4-to-6-letter words
+grep -E '^[a-z]{4,6}$' $DICT_FILE >$WORDS_IN
+
+# Compile the Java - not really needed every time, but hey.
+javac -d bin src/net/robcranfill/combofinder/ComboFinder.java
+
+java -classpath .:bin net.robcranfill.combofinder.ComboFinder $WORDS_IN $WORDS_OUT
+
+# Create the HTML
+cat combo.html.1  > $HTML_OUT
+cat $WORDS_OUT   >> $HTML_OUT
+cat combo.html.2 >> $HTML_OUT
